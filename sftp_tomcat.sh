4b190e8d12a135926d6a668d76a2a41171e5d356
saranaravi <37202787+saranaravi@users.noreply.github.com>
2018-04-14 21:57:11 +0530

    Update sftp_tomcat.sh

--- sftp_tomcat.sh
+++ sftp_tomcat.sh
@@ -1,11 +1,8 @@
 mv target/myweb*.war target/myweb.war
 
-sftp ec2-user@172.31.17.24 <<EOF
-
-rename /opt/tomcat8/webapps/myweb.war /tmp/myweb.war.back
-rm /opt/tomcat8/webapps/myweb*
+sftp ec2-user@13.126.215.242 <<EOF
+rename /tomcat8/webapps/myweb.war /tmp/myweb.war.back
+rm /tomcat8/webapps/myweb*
 put target/myweb.war  /opt/tomcat8/webapps/
 bye
 EOF
-ssh ec2-user@172.31.17.24 /opt/tomcat8/bin/shutdown.sh
-ssh ec2-user@172.31.17.24 /opt/tomcat8/bin/startup.sh
